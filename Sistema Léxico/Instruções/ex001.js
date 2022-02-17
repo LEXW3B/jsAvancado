d950c7c289878b9865e7bb42137eb63181b05932
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-17 08:09:22 -0300

    sistema lexico

---
+++ Sistema Léxico/Instruções/ex001.js
@@ -0,0 +1,37 @@
+//instruções é tudo que e passado linha a linha.
+let nome = 'Alexandre' //qualquer comando no js é uma instrução
+console.log(nome)
+
+//caseSensetive = é o nome dado no js a diferenca entre letras maiusculas e minusculas
+let nome1 = 'Alexandre'
+console.log(Nome1)//a letra ('N' e diferente de 'n') então não vai funcionar. 
+
+/*             PALAVRAS RESERVADAS
+
+break          debugger          finally          new          typeof 
+case           default           for              return       var
+catch          delete            function         super        void
+class          do                if               switch       while
+const          else              import           this         with
+continue       export            in               throw        yield
+               extends           instanceof       try  
+O uso do ';' em uma lina no js somente em casos de duas instruções na mesma linha.
+Funções simples uso construtor de nomes : começa com letra minuscula e a segunda palavra começa com maiuscula tudo junto. ex:. camelCase , getElement, fuiViajar... isso é o padrão camelCase. É não pode começar com número nem caracteres especiais. ex:. !@#$%¨&*(°)
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
