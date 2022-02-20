6e5112ce11f96d35a5d1a99306520fdd4d01a9b6
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-02-20 18:49:37 -0300

    hoinsting

---
+++ Funções/hoisting.js
@@ -0,0 +1,32 @@
+//Funções em Js - são tipos de objetos com a incrivel capacidade de serem invocados
+function sair () {
+    console.log()
+}
+
+//hoinsting - capacidade do javascript de 'içar' uma function declaration
+function teste () {
+    console.log('teste')
+}
+teste()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
