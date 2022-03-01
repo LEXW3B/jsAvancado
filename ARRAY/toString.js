49ab16cdba86df6349b50028156c473116bc5f65
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-03-01 15:54:10 -0300

    toString

---
+++ ARRAY/toString.js
@@ -0,0 +1,14 @@
+//o metodo toString vai transformar toda a array em string e mostrar entre virgulas
+let arr1 = [1,2,3]
+let arr2 = [4,5,6]
+console.log(arr1.toString())
+console.log(typeof arr1.toString())
+
+
+
+
+
+
+
+
+
