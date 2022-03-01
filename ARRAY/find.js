db80dda17a72262adf9c900c61faf52fc777197f
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-03-01 15:44:00 -0300

    find

---
+++ ARRAY/find.js
@@ -0,0 +1,17 @@
+//ele vai encontrar o primeiro retorno que for true, se nao encontrar nada sera undefined
+
+let arr = [4,5,10,20,35,4,5]
+console.log(arr.find(function(el){
+    return el > 10
+}))   
+
+
+
+
+
+
+
+
+
+
+
