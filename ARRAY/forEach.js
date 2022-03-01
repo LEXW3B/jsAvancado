5f5f64a4ba940f0307c94e9a934b488bb25c8aa2
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-03-01 15:19:15 -0300

    forEach

---
+++ ARRAY/forEach.js
@@ -0,0 +1,18 @@
+//com o metodo forEach ele sempre vai retornar undefined fora da função
+
+const arr = [1,5,10,'ola',true]
+
+const arr1 = arr.forEach(function(el,i, _arr){
+    console.log(el)
+})
+console.log(arr1)
+
+
+
+
+
+
+
+
+
+
