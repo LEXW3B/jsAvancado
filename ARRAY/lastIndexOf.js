2852155e51cb5f7ff8307a51495a2e078ecacdd3
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-03-01 15:36:38 -0300

    vgdsa

---
+++ ARRAY/lastIndexOf.js
@@ -0,0 +1,21 @@
+//o metodo lastIndexOf vai retornar a ultima posição do indice do numero procurado mesmo se tive ele repetido na array, se nao houver o numero que esta procurando ele sempre vai retornar -1
+
+let arr = [4,5,10,20,35,4,5]
+console.log(arr.lastIndexOf(4))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
