dfd9925be3cb338a2c2c32b0c3c21f480d9ecc2d
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-03-01 15:48:36 -0300

    findIndex

---
+++ ARRAY/findIndex.js
@@ -0,0 +1,16 @@
+//retorna o numero do indice que e a resposta do retorno nesse exemplo vai ser o numero 20 sendo o indice 3 , se nao existir o numero retornado sera -1
+
+let arr = [4,5,10,20,35,4,5]
+console.log(arr.findIndex(function(el){
+    return el>10
+}))
+
+
+
+
+
+
+
+
+
+
