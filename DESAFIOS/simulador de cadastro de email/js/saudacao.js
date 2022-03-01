b819b8fcc12cd13d3c51ca4b471ad8b7bddc703e
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-03-01 08:15:31 -0300

    envio

--- DESAFIOS/simulador de cadastro de email/js/saudacao.js
+++ DESAFIOS/simulador de cadastro de email/js/saudacao.js
@@ -1,4 +1,7 @@
-(function () {
+const receberEmail = document.querySelector('#txtEmail')
+const resp = document.querySelector('#newsletterFeedback')
 
-
-})()
+function envio () {        
+    let email = receberEmail.value
+    resp.innerHTML = `O seu email: ${email} já foi cadastrado com sucesso.`
+}
