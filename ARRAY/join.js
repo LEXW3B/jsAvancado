374ca05113beeefd9f05e05d7e6fe34ca9a2b47b
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-03-01 15:58:44 -0300

    join

---
+++ ARRAY/join.js
@@ -0,0 +1,5 @@
+//o metodo join vai fazer a mesma coisa do toString . mais por parametro da pra passar o caractere que quiser para separar os numeros
+let arr1 = [1,2,3]
+let arr2 = [4,5,6]
+console.log(arr1)
+console.log(arr1.join(' - '))
