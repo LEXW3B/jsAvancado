9a0fb9bde709ed4d1372133cba72fc0139eb550b
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-03-01 10:13:11 -0300

    funciona

--- DESAFIOS/habilitar e desabilitar input/js/newsletter.js
+++ DESAFIOS/habilitar e desabilitar input/js/newsletter.js
@@ -1,4 +1,10 @@
 const txtEmail = document.getElementById("txtEmail")
 
 
-
+function funcionarEditar () {
+    txtEmail.disabled = false
+    txtEmail.focus()
+}
+function naoFuncionarEditar () {
+    txtEmail.disabled = true
+}
