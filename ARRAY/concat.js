45389dc0c498c20b4b55b3566ab550e9ce964171
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-03-01 16:06:40 -0300

    concat

---
+++ ARRAY/concat.js
@@ -0,0 +1,24 @@
+//vai concatenar as duas arrays. mas transforma as arrays em objeto
+let arr1 = [1,2,3]
+let arr2 = [4,5,6]
+
+let arr3 = arr1.concat(arr2, 7,8,100, 89, ['ola', 'mundo'])
+console.log(arr3)
+console.log(typeof arr3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
