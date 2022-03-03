c2af0432b8979d5ee51697b2899b38cb4021ff7d
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-03-03 15:14:47 -0300

    sd

---
+++ ARRAY/slice.js
@@ -0,0 +1,14 @@
+//o metodo slice - ele recorta elementos selecionados por indices por parametro
+let arr = [1,3,5,7,9,12,22,55,44]
+let test1 = arr.slice(1,4)
+console.log(arr)
+console.log(test1)
+
+
+
+
+
+
+
+
+
