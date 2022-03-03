943f344b557f79c3bf7c9a7b421caec52acca5e3
LEXW3B <78969878+LEXW3B@users.noreply.github.com>
2022-03-03 15:09:10 -0300

    UN

---
+++ ARRAY/shiftUnshift.js
@@ -0,0 +1,23 @@
+//o metodo shift - remove o primeiro elemento da array 
+//o metodo unshift - adiciona elemento no inicio da array
+let arr = [1,3,5,7,9]
+
+let test1 = arr.shift()
+console.log(test1)
+console.log(arr)
+
+let test2 = arr.unshift(4,5,6)
+console.log(arr)
+console.log(test2)
+
+
+
+
+
+
+
+
+
+
+
+
